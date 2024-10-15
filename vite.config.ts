f0fa680895891d948ed5e5a377f8bcc2f9cb0b42
jurgob <jurgob@users.noreply.github.com>
2024-10-15 22:51:52 +0100

    different target for ssr

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
   plugins: [
     remix({
       future: {
@@ -13,4 +13,5 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-});
+  build: isSsrBuild ? { target: 'ES2022' } : {},
+}));
