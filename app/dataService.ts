2674eb0dcc6ca310191e00712bf498753f713217
jurgob <jurgob@users.noreply.github.com>
2024-10-23 00:19:31 +0100

    migrate in the code

--- app/dataService.ts
+++ app/dataService.ts
@@ -1,12 +1,12 @@
-import {db} from "./db/db";
-import {migrateTodoSchema} from "./db/migrate";
+import {dbClient} from "./db/db";
 import { todoTable} from "./db/schema";
 import {env} from "./env";
 
 export async function createDataService() {
+    const {db, migrate} = dbClient;
     console.log("creating data service");
     console.log(`env.DATABASE_URL `, env.DATABASE_URL);
-    await migrateTodoSchema();
+    await migrate();
     
     const createTodo = async (todo: typeof todoTable.$inferInsert) => {
         return (await db.insert(todoTable).values(todo).returning().execute())[0];   
--- app/db/db.ts
+++ app/db/db.ts
@@ -1,8 +1,17 @@
 import { PGlite } from '@electric-sql/pglite';
 import { drizzle as drizzlePgLite } from 'drizzle-orm/pglite';
+import { migrate as pbLiteMitrate } from "drizzle-orm/pglite/migrator"
+
+
 import { drizzle } from "drizzle-orm/node-postgres";
 import pkg from 'pg';
 const {Pool} = pkg;
+import { migrate  as pgNodMigrate} from "drizzle-orm/node-postgres/migrator"
+
+const MIGRATE_CONFIG =  {
+    migrationsFolder: "drizzle"
+}
+
 // connectionString
 import {env} from "../env";
 
@@ -10,17 +19,29 @@ function createDb() {
     if(env.DATABASE_URL === ":inmemory:") {
         const client = new PGlite();
         const db = drizzlePgLite(client);
-        return db;
+        const migrate = async () => {
+            return await pbLiteMitrate(db,MIGRATE_CONFIG);
+        }
+        return {
+            db,
+            migrate
+        };
     }else {
         const pool = new Pool({
             connectionString: env.DATABASE_URL
         });
         const db = drizzle(pool);
-        return db;
+        const migrate = async () => {
+            return await pgNodMigrate(db,MIGRATE_CONFIG);
+        }
+        return { 
+            db,
+            migrate
+        };
     }
     
 }
 
 
-export const db = createDb();
+export const dbClient = createDb();
 
